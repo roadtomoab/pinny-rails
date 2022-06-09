c6abc7ffbd37f68db8e6b231c2544193d584d897
Noah Blumenstein <noah.blumenstein@gmail.com>
2022-06-08 21:09:17 -0400

    most pin controller actions done

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,2 +1,52 @@
 class PinsController < ApplicationController
+    before_action :authorize
+    skip_before_action :authorize, only: [:index]
+
+
+    def index
+        render json: Pin.all
+    end
+
+    def show
+        pin = Pin.find_by_id(params[:id])
+        if pin
+            render json: pin, status: :ok
+        else
+            render_not_found_message
+        end
+    end
+
+    def create
+        user_id = session[:user_id]
+        params[:user_id] = user_id
+        if pin.valid?
+            render json: pin, status: :created
+        else
+            render json: { errors: pin.errors.full_message }, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        pin = Pin.find_by_id(params[:id])
+        if pin
+            pin.destroy
+            head :no_content
+        else
+            render_not_found_message
+        end
+    end
+
+    private
+
+    def authorize
+        return render json: { error: "Must be logged in to drop a pin" }, status: :unauthorized unless session.include? :user_id
+    end
+
+    def pin_params
+        params.permit(:title, :desc, :latitude, :longitude, :user_id)
+    end
+
+    def render_not_found_message
+        render json: { error: "pin not found" }
+    end
 end
--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,4 +1,4 @@
 class Pin < ApplicationRecord
   belongs_to :user
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
 end
